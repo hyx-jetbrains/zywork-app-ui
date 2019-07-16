671f9a4f831ad3b0a7f003171bd7d43169a8e450
WangZhenyu <847315251@qq.com>
2019-07-16 15:41:14 +0800

    更新视图

--- iview-admin/src/view/goods-category-attr/GoodsCategoryAttributeMainModal.vue
+++ iview-admin/src/view/goods-category-attr/GoodsCategoryAttributeMainModal.vue
@@ -2,6 +2,10 @@
     <div>
         <Modal v-model="categoryAttributeModal" title="商品类目属性" width="80%">
             <GoodsCategoryAttributeMain ref="goodsCategoryAttributeMain" @closeModal="closeModal"/>
+             <div slot="footer">
+                <Button type="text" size="large" @click="closeModal">取消</Button>
+                <Button type="primary" size="large" @click="setAttrGroup">确定</Button>
+            </div>
         </Modal>
     </div>
 </template>
@@ -24,6 +28,9 @@
         methods: {
             closeModal() {
                 this.categoryAttributeModal = false
+            },
+            setAttrGroup() {
+                this.$refs.goodsCategoryAttributeMain.setAttrGroup()
             }
         }
     }
