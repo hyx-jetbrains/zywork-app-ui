9e063be97982b6c6f53728f748ac1b48ebc48646
Carter <wjhsmarter@126.com>
2019-07-22 16:12:16 +0800

    优化 配置商品和商品SKU的分销比例

--- iview-admin/src/components/distribution-ratio-modal/distribution-ratio-modal.vue
+++ iview-admin/src/components/distribution-ratio-modal/distribution-ratio-modal.vue
@@ -1,17 +1,17 @@
 <template>
-  <Modal v-model="setModal" :title="title" :mask-closable="false" width="560"> 
+  <Modal v-model="setModal" :title="title" :mask-closable="false" width="560">
     <div class="zy-box">
       <div class="zy-box-row">
         <div class="zy-text-bold">角色/等级</div>
         <div class="zy-text-bold" v-for="(item, index) in levelArr" :key="index">{{item}}级</div>
-        <p style="clear:both"></p> 
+        <p style="clear:both"></p>
       </div>
       <div class="zy-box-row" v-for="(item, index) in ratioData" :key="index">
         <div class="zy-text-bold">{{rolesArr[index].title}}</div>
         <div v-for="(tempItem, index_1) in levelArr" :key="index_1">
           <Input class="zy-input" placeholder="输入分销比例" v-model="ratioData[index][index_1]" />
         </div>
-        <p style="clear:both"></p> 
+        <p style="clear:both"></p>
       </div>
     </div>
     <div slot="footer">
@@ -22,15 +22,20 @@
 </template>
 
 <script>
-
+import * as utils from '@/api/utils-v2'
+import * as ResponseStatus from '@/api/response-status'
 export default {
   name: 'DistributionRatioModal',
-  props: {
-  },
-  components: {
-  },
-  data () {
+  props: {},
+  components: {},
+  data() {
     return {
+      urls: {
+        batchSaveUrl: '/goods-commission/admin/remove-and-batch-save/',
+        listCommissionUrl: '/goods-commission/admin/all-cond',
+        listRoleUrl: '/role/admin/list-distribution-roles',
+        distributionLevelUrl: '/distribution/admin/distribution-level'
+      },
       setModal: false,
       setLoading: false,
       rolesArr: [],
@@ -39,7 +44,9 @@ export default {
       title: '',
       goodsId: null,
       goodsSkuId: null,
-      
+      type: null,
+      id: null,
+      validFlag: true
     }
   },
   methods: {
@@ -48,7 +55,27 @@ export default {
      */
     setDistributionRatio() {
       const listData = this.setData()
-      this.$emit('setDistributionRatio', listData)
+      if (this.validFlag) {
+        this.setLoading = true
+        utils
+          .doPostJson(
+            this.urls.batchSaveUrl + this.type + '/' + this.id,
+            listData,
+            {}
+          )
+          .then(res => {
+            this.setLoading = false
+            if (ResponseStatus.OK === res.data.code) {
+              this.setModal = false
+              this.$Message.success('分配成功')
+            } else {
+              this.$Message.error(res.data.message)
+            }
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      }
     },
     /**
      * 组装数据
@@ -59,12 +86,22 @@ export default {
       const levelLen = this.levelArr.length
       for (var i = 0; i < ratioDataLen; i++) {
         const roleId = this.rolesArr[i].id
-        
         for (var j = 0; j < levelLen; j++) {
-          const val = this.ratioData[i][j]
+          const val = parseInt(this.ratioData[i][j])
           if (!val) {
+            this.validFlag = false
             this.$Message.error('请确保所有数据的完整性')
-            return;
+            return
+          }
+          if (val < 0) {
+            this.validFlag = false
+            this.$Message.error('比例不能小于0')
+            return
+          }
+          if (val > 100) {
+            this.validFlag = false
+            this.$Message.error('比例不能大于100')
+            return
           }
           listData.push({
             goodsId: this.goodsId,
@@ -72,15 +109,91 @@ export default {
             roleId: roleId,
             distributionLevel: this.levelArr[j],
             commissionPercent: val
-          
           })
         }
       }
+      this.validFlag = true
       return listData
     },
+    /**
+     * 请求后台接口，获取数据
+     */
+    loadData(param, type, id) {
+      if (param.goodsId) {
+        this.goodsId = param.goodsId
+      }
+      if (param.goodsSkuId) {
+        this.goodsSkuId = param.goodsSkuId
+      }
+      this.type = type
+      this.id = id
+      this.setModal = true
+      // 先请求分销比例表
+      utils
+        .doPostJson(this.urls.listCommissionUrl, param, {})
+        .then(res => {
+          if (ResponseStatus.OK === res.data.code) {
+            // 成功获取分销比例列表，继续获取角色
+            let commissionList = res.data.data.rows
+            utils
+              .doGet(this.urls.listRoleUrl, {})
+              .then(res1 => {
+                if (ResponseStatus.OK === res1.data.code) {
+                  // 成功获取角色列表，继续获取分销等级
+                  let roleList = res1.data.data.rows
+                  utils
+                    .doGet(this.urls.distributionLevelUrl, {})
+                    .then(res2 => {
+                      if (ResponseStatus.OK === res2.data.code) {
+                        // 成功获取分销等级，开始处理数据
+                        let level = res2.data.data - 1
+                        let levelArr = []
+                        for (var i = 0; i < level; i++) {
+                          levelArr.push(1 + i)
+                        }
+                        let rolesArr = []
+                        let data = []
+                        const commissionLen = commissionList.length
+                        const roleLen = roleList.length
+                        for (var i = 0; i < roleLen; i++) {
+                          let roleItem = roleList[i]
+                          let itemData = []
+                          for (var j = 0; j < commissionLen; j++) {
+                            let commissionItem = commissionList[j]
+                            if (roleItem.id === commissionItem.roleId) {
+                              itemData.push(commissionItem.commissionPercent)
+                            }
+                          }
+                          rolesArr.push(roleItem)
+                          data.push(itemData)
+                        }
+                        this.rolesArr = rolesArr
+                        this.levelArr = levelArr
+                        this.ratioData = data
+                      } else {
+                        this.$Message.error(res2.data.message)
+                      }
+                    })
+                    .catch(err2 => {
+                      console.log(err2)
+                    })
+                } else {
+                  this.$Message.error(res1.data.message)
+                }
+              })
+              .catch(err1 => {
+                console.log(err1)
+              })
+          } else {
+            this.$Message.error(res.data.message)
+          }
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
   },
-  mounted () {
-  }
+  mounted() {}
 }
 </script>
 
@@ -89,26 +202,26 @@ export default {
   font-weight: bold;
 }
 .zy-box {
-  border: 1px solid #CCC;
+  border: 1px solid #ccc;
   text-align: center;
   width: 100%;
 }
 
 .zy-box-row {
-  border-bottom: 1px solid #CCC;
+  border-bottom: 1px solid #ccc;
   padding: 10px;
 }
 .zy-box-row:last-child {
   border-bottom: none;
 }
 .zy-box-row div {
-  border-right: 1px solid #CCC;
+  border-right: 1px solid #ccc;
   float: left;
-  width: 150px;
+  width: 168px;
   padding: 0px 20px 0px 5px;
 }
 .zy-box-row div:last-child {
-  border-right: none;
+  border-right: 0;
 }
 
 .zy-input input {
--- iview-admin/src/view/goods-sku/GoodsSkuMain.vue
+++ iview-admin/src/view/goods-sku/GoodsSkuMain.vue
@@ -40,10 +40,7 @@
     <GoodsSkuDetailModal ref="detailModal" />
     <GoodsSkuPicModal ref="goodsSkuPicModal" v-on:searchTable="searchTable" />
     <SkuDetailModal ref="skuDetailModal" />
-    <DistributionRatioModal
-      ref="distributionRatioModal"
-      v-on:setDistributionRatio="setDistributionRatio"
-    />
+    <DistributionRatioModal ref="distributionRatioModal" />
   </div>
 </template>
 
@@ -72,11 +69,7 @@ export default {
     return {
       urls: {
         batchRemoveUrl: '/goods-sku/admin/batch-remove',
-        batchActiveUrl: '/goods-sku/admin/batch-active',
-        batchSaveUrl: '/goods-commission/admin/batch-save',
-        listCommissionUrl: '/goods-commission/admin/all-cond',
-        listRoleUrl: '/role/admin/list-distribution-roles',
-        distributionLevelUrl: '/distribution/admin/distribution-level'
+        batchActiveUrl: '/goods-sku/admin/batch-active'
       }
     }
   },
@@ -152,108 +145,14 @@ export default {
      * 显示分销比例配置弹窗
      */
     showDistributionRatio(row) {
-      const goodsId = row.goodsId
-      const goodsSkuId = row.id
       let distributionRatioModal = this.$refs.distributionRatioModal
-      distributionRatioModal.setModal = true
-      distributionRatioModal.goodsId = goodsId
-      distributionRatioModal.goodsSkuId = goodsSkuId
       distributionRatioModal.title = '配置商品SKU的分销佣金比例'
       const params = {
-        goodsId: goodsId,
-        goodsSkuId: goodsSkuId,
+        goodsId: row.goodsId,
+        goodsSkuId: row.id,
         isActive: 0
       }
-      this.loadData(params, distributionRatioModal)
-    },
-    /**
-     * 请求后台接口，获取数据
-     */
-    loadData(param, distributionRatioModal) {
-      // 先请求分销比例表
-      utils
-        .doPostJson(this.urls.listCommissionUrl, param, {})
-        .then(res => {
-          if (ResponseStatus.OK === res.data.code) {
-            // 成功获取分销比例列表，继续获取角色
-            let commissionList = res.data.data.rows
-            utils
-              .doGet(this.urls.listRoleUrl, {})
-              .then(res1 => {
-                if (ResponseStatus.OK === res1.data.code) {
-                  // 成功获取角色列表，继续获取分销等级
-                  let roleList = res1.data.data.rows
-                  utils
-                    .doGet(this.urls.distributionLevelUrl, {})
-                    .then(res2 => {
-                      if (ResponseStatus.OK === res2.data.code) {
-                        // 成功获取分销等级，开始处理数据
-                        let level = res2.data.data - 1
-                        let levelArr = []
-                        for (var i = 0; i < level; i++) {
-                          levelArr.push(1 + i)
-                        }
-                        let rolesArr = []
-                        let data = []
-                        const commissionLen = commissionList.length
-                        const roleLen = roleList.length
-                        for (var i = 0; i < roleLen; i++) {
-                          let roleItem = roleList[i]
-                          let itemData = []
-                          for (var j = 0; j < commissionLen; j++) {
-                            let commissionItem = commissionList[j]
-                            if (roleItem.id === commissionItem.roleId) {
-                              itemData.push(commissionItem.commissionPercent)
-                            }
-                          }
-                          rolesArr.push(roleItem)
-                          data.push(itemData)
-                        }
-                        distributionRatioModal.rolesArr = rolesArr
-                        distributionRatioModal.levelArr = levelArr
-                        distributionRatioModal.ratioData = data
-                      } else {
-                        this.$Message.error(res2.data.message)
-                      }
-                    })
-                    .catch(err2 => {
-                      console.log(err2)
-                    })
-                } else {
-                  this.$Message.error(res1.data.message)
-                }
-              })
-              .catch(err1 => {
-                console.log(err1)
-              })
-          } else {
-            this.$Message.error(res.data.message)
-          }
-        })
-        .catch(err => {
-          console.log(err)
-        })
-    },
-    /**
-     * 设置分销比例
-     */
-    setDistributionRatio(listData) {
-      let distributionRatioModal = this.$refs.distributionRatioModal
-      distributionRatioModal.setLoading = true
-      utils
-        .doPostJson(this.urls.batchSaveUrl, listData, {})
-        .then(res => {
-          distributionRatioModal.setLoading = false
-          if (ResponseStatus.OK === res.data.code) {
-            distributionRatioModal.setModal = false
-            this.$Message.success('分配成功')
-          } else {
-            this.$Message.error(res.data.message)
-          }
-        })
-        .catch(err => {
-          console.log(err)
-        })
+      distributionRatioModal.loadData(params, 'goodsSku', row.id)
     }
   }
 }
